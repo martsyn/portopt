fe29230f21874eeb8fb29a99478b937d8201ae67
martsyn <martsyn@github>
2016-02-10 15:05:16 -0500

    moved secret to separate php

--- www/mailchimp/complete.php
+++ www/mailchimp/complete.php
@@ -6,9 +6,7 @@
 
 require_once(__DIR__.'/../api/common.php');
 require_once('MailchimpDatabase.php');
-
-$clientId = '755469067161';
-$clientSecret = 'b7cf12433cb3939b40fbf2edf45f341c';
+require_once(__DIR__.'/../api/secrets.php');
 
 $apiKey = NULL;
 
@@ -17,7 +15,7 @@ if (!$code) {
     $error = 'Code was not supplied';
 }
 else {
-    $data = "grant_type=authorization_code&client_id=$clientId&client_secret=$clientSecret&redirect_uri=https%3A%2F%2Fapi.hfinone.com%2Fmailchimp%2Fcomplete.php&code=$code";
+    $data = "grant_type=authorization_code&client_id=$mailchimpClientId&client_secret=$mailchimpClientSecret&redirect_uri=https%3A%2F%2Fapi.hfinone.com%2Fmailchimp%2Fcomplete.php&code=$code";
     $response = httpRequest('POST', 'https://login.mailchimp.com/oauth2/token', NULL, NULL, $data);
     if (!$response) {
         $error = 'Code received successfully, but token request failed';
